12e62b55afe68d53dfbd1eed0f7faf085a8b42cb
Jaeyoung Lim <limjyjustin@gmail.com>
2023-07-03 16:12:43 +0900

    Add files via upload

---
+++ src/main/java/dev/umc/whereseat/WhereseatApplication.java
@@ -0,0 +1,13 @@
+package dev.umc.whereseat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhereseatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhereseatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/umc/whereseat/WhereseatApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.umc.whereseat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhereseatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
