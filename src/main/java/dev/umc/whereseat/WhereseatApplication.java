341c31e00498b3b5fb8c007c6fa98e4100c1e893
zer0silver <duddms0_0@naver.com>
2023-07-03 23:21:13 +0900

    fix: @EnableJpaAuditing 추가

--- src/main/java/dev/umc/whereseat/WhereseatApplication.java
+++ src/main/java/dev/umc/whereseat/WhereseatApplication.java
@@ -2,8 +2,10 @@ package dev.umc.whereseat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class WhereseatApplication {
 
 	public static void main(String[] args) {
