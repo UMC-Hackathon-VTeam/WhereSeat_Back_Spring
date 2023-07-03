8fa43b2717ef7b1f53c48d36924a5926335d76b2
zer0silver <duddms0_0@naver.com>
2023-07-03 23:23:08 +0900

    fix: 쿼리에 작성자 추가

--- src/main/java/dev/umc/whereseat/domain/diary/DiaryRepository.java
+++ src/main/java/dev/umc/whereseat/domain/diary/DiaryRepository.java
@@ -1,5 +1,6 @@
 package dev.umc.whereseat.domain.diary;
 
+import dev.umc.whereseat.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
@@ -8,7 +9,7 @@ import java.util.Optional;
 
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
     Optional<Diary> findByVisitedAt(LocalDate date);
-    List<Diary> findAllByVisitedAtBetweenOrderByVisitedAt(LocalDate startDate, LocalDate endDate);
+    List<Diary> findAllByMemberAndVisitedAtBetweenOrderByVisitedAt(Member member, LocalDate startDate, LocalDate endDate);
 
 
 }
